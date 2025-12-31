fb886b2afd4ec34d95215d63f5ce805d5ec9dab8
Yasin0404 <ahmetcatalkaya04@gmail.com>
2025-12-31 04:58:06 +0300

    Add files via upload

---
+++ APITest/APITest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using EvOtomasyonu;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--- API TEST PROGRAMI ---");
+
+        EvOtomasyonu.AirConditionerSystemConnection klima = new EvOtomasyonu.AirConditionerSystemConnection();
+
+
+        klima.SetComPort(1);
+        klima.SetBaudRate(9600);
+
+        Console.WriteLine("Bağlantı deneniyor...");
+        if (klima.Open())
+        {
+            Console.WriteLine("BAŞARILI: Port açıldı.");
+
+            Console.WriteLine("Sıcaklık 25.5 olarak ayarlanıyor...");
+
+            bool sonuc = klima.setDesiredTemp(25.5f);
+
+            if (sonuc) Console.WriteLine("BAŞARILI: Veri gönderildi.");
+            else Console.WriteLine("HATA: Veri gönderilemedi.");
+
+            klima.Close();
+        }
+        else
+        {
+            Console.WriteLine("HATA: Port açılamadı");
+        }
+
+        Console.ReadLine();
+    }
+}
